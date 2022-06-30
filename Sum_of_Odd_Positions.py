4743ec2a76b61232ea691b86bd74af0d486a8514
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2022-06-30 20:48:02 +0530

    osum=0 n = int(input()) x = list(map(int,input().split()))
    for i in range(n):
        if i%2==1:
            osum+=x[i]
    
            
    print(osum)

--- Sum_of_Odd_Positions.py
+++ Sum_of_Odd_Positions.py
@@ -1,10 +1,10 @@
-oddsum=0
+osum=0
 n = int(input())
 x = list(map(int,input().split()))
 
 for i in range(n):
     if i%2==1:
-        oddsum+=x[i]
+        osum+=x[i]
 
         
-print(oddsum)
+print(osum)
