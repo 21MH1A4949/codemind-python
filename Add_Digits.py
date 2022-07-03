c613b6534ca57dfb232cb87cb1fbd11991aa905d
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2022-07-03 11:44:37 +0530

    n=int(input()) temp=n while True:     s=0     while temp:         s+=temp%10         temp//=10     if len(str(s))==1:         print(s)         break     else:         temp=s

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,17 +1,12 @@
 n=int(input())
 temp=n
 while True:
-    dcount=0
     s=0
     while temp:
         s+=temp%10
-        dcount+=1
         temp//=10
-    if dcount==1:
+    if len(str(s))==1:
         print(s)
         break
     else:
         temp=s
-    
-    
-    
