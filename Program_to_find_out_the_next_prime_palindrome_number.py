4fd00ccdc0774055b2bf6786e77b3633780a29b7
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2022-07-09 08:14:52 +0530

    def prime(n):     if n==1:         return False     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False     return True
    def palin(n):
        temp = n
        rev = 0
        while temp:
            rev = rev*10 + temp%10
            temp//=10
        if rev==n:
            return True
        return False
    
    n = int(input())
    after = n+1
    while after:
        if prime(after) and palin(after):
            print(after)
            break
        after+=1

--- Program_to_find_out_the_next_prime_palindrome_number.py
+++ Program_to_find_out_the_next_prime_palindrome_number.py
@@ -1,24 +1,25 @@
 def prime(n):
     if n==1:
         return False
-    for i in range(2,int(n**.5)+1):
+    for i in range(2,int(n**0.5)+1):
         if n%i==0:
             return False
     return True
-def palindrome(n):
-    temp=n
-    rev=0
+
+def palin(n):
+    temp = n
+    rev = 0
     while temp:
-        rev=rev*10+temp%10
+        rev = rev*10 + temp%10
         temp//=10
     if rev==n:
         return True
     return False
-n=int(input())
-after=n+1
+
+n = int(input())
+after = n+1
 while after:
-    if prime(after) and palindrome(after):
+    if prime(after) and palin(after):
+        print(after)
         break
     after+=1
-print(after)
-    
