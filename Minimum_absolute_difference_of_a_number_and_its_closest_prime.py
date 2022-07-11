7dedd3fe67a5796cc2384d8d77190eb4fc3b5995
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2022-07-11 14:26:22 +0530

    def prime(n):     if n==1:         return False     for i in range(2,int(n**.5)+1):         if n%i==0:             return False     return True
    n=int(input())
    if prime(n):
        print(0)
    after=n
    before=n
    while after:
        if prime(after):
            break
        after+=1
        
    while before:
        if prime(before):
            break
        before-=1
        
        diff=after-n
        diff1=n-before
    if diff<diff1:
        print(abs(diff))
    elif diff>diff1:
        print(diff1)
    else:
        print(abs(diff1))

--- Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -6,26 +6,27 @@ def prime(n):
             return False
     return True
     
+    
 n=int(input())
 if prime(n):
     print(0)
-else:
-    after=n+1
-    before=n-1
-    while after:
-        if prime(after):
-            break
-        after+=1
-    while before:
-        if prime(before):
-            break
-        before-=1
-        
+after=n
+before=n
+while after:
+    if prime(after):
+        break
+    after+=1
+    
+while before:
+    if prime(before):
+        break
+    before-=1
+    
     diff=after-n
     diff1=n-before
-    
-    if diff>diff1:
-        print(diff1)
-        
-    else:
-        print(diff)
+if diff<diff1:
+    print(abs(diff))
+elif diff>diff1:
+    print(diff1)
+else:
+    print(abs(diff1))
