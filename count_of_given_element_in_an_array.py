bbad2bb341280a2e6e276ac37f73736c33b09fb1
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2022-07-11 23:59:13 +0530

    n=int(input()) x=list(map(int,input().split())) z=int(input()) k=x.count(z) print(k)

--- count_of_given_element_in_an_array.py
+++ count_of_given_element_in_an_array.py
@@ -1,9 +1,5 @@
 n=int(input())
-arr=list(map(int,input().split()))
+x=list(map(int,input().split()))
 z=int(input())
-c=0
-for i in arr:
-    if z==i:
-        c+=1
-print(c)        
-        
+k=x.count(z)
+print(k)
