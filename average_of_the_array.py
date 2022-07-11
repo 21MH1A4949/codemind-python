c5027b628f5f66ab459366b3537a22884a9be166
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2022-07-11 23:48:33 +0530

    n=int(input()) x=list(map(int,input().split())) k=sum(x) avg=k/n print("{:.2f}".format(avg))

--- average_of_the_array.py
+++ average_of_the_array.py
@@ -1,7 +1,5 @@
-a = int(input())
-
-x = list(map(int,input().split()))
-sum_ = 0
-for i in x:
-    sum_+=i
-print('{:.2f}'.format(sum_/a))
+n=int(input())
+x=list(map(int,input().split()))
+k=sum(x)
+avg=k/n
+print("{:.2f}".format(avg))
