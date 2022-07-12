0b49cdeb7e2bc0bd1d37b1944f99c0817537295c
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2022-07-12 14:38:06 +0530

    n=int(input()) x=list(map(int,input().split())) s=sum(x) avg=s//n c=0 for i in x:     if i<=avg:         c+=1 print(c)

--- elements_smaller_than_average.py
+++ elements_smaller_than_average.py
@@ -1,12 +1,9 @@
 n=int(input())
 x=list(map(int,input().split()))
-# print(x)
-# print(x)
-c=0
 s=sum(x)
 avg=s//n
-for i in range(n):
-    if x[i]<=avg:
+c=0
+for i in x:
+    if i<=avg:
         c+=1
-        # print(i)
 print(c)        
