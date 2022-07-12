990ba189bcd85b831832828554f0c123e5abf213
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2022-07-12 14:31:05 +0530

    n=int(input()) x=list(map(int,input().split())) # print(x) for i in range(n):     if x[i]%2!=0:         print(False)         break else:     print(True)

--- Even_Array.py
+++ Even_Array.py
@@ -1,9 +1,10 @@
 n=int(input())
 x=list(map(int,input().split()))
 # print(x)
-for i in x:
-    if i%2==1:
+for i in range(n):
+    if x[i]%2!=0:
         print(False)
         break
 else:
     print(True)
+        
