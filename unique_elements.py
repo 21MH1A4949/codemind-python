f00cf4fb808d04ce85ff3c10e426ebd3997e32e0
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2022-07-12 15:01:40 +0530

    n=int(input()) x=list(map(int,input().split())) c=[]
    for i in x:
        if i not in c:
            c.append(i)
            print(i,end=" ")

--- unique_elements.py
+++ unique_elements.py
@@ -1,8 +1,8 @@
-n = int(input())
-x = list(map(int,input().split()))
-r = []
+n=int(input())
+x=list(map(int,input().split()))
+c=[]
 
 for i in x:
-    if i not in r:
-        r.append(i)
-        print(i,end=' ')
+    if i not in c:
+        c.append(i)
+        print(i,end=" ")
