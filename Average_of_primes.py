13c15a8ff0ec20b91fec156c488ab658a3ac491e
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2022-07-12 12:24:23 +0530

    def prime(n):     if n==1:         return False     for i in range(2,int(n**.5)+1):         if n%i==0:             return False     return True
    n=int(input())
    x=list(map(int,input().split()))
    s=0
    c=0
    for i in x:
        if prime(i):
            s+=i
            c+=1
            avg=s/c
    print("{:.2f}".format(avg))

---
+++ Average_of_primes.py
@@ -0,0 +1,18 @@
+def prime(n):
+    if n==1:
+        return False
+    for i in range(2,int(n**.5)+1):
+        if n%i==0:
+            return False
+    return True
+    
+n=int(input())
+x=list(map(int,input().split()))
+s=0
+c=0
+for i in x:
+    if prime(i):
+        s+=i
+        c+=1
+        avg=s/c
+print("{:.2f}".format(avg))        
