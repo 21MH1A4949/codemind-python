6eacb62ece91191a212dcdd2f7dcd3b6a7f539e4
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2022-07-13 18:16:50 +0530

    n,m=map(int,input().split()) n1=list(map(int,input().split())) m1=list(map(int,input().split())) temp=[] c=0 for i in n1:     if (i in m1) and (i not in temp):         temp.append(i)         c+=1 print(c)

--- Count_elements_I.py
+++ Count_elements_I.py
@@ -1,5 +1,10 @@
-a,b=map(int,input().split())
-n=set(list(map(int,input().split())))
-m=set(list(map(int,input().split())))
-s=n.intersection(m)
-print(len(s))
+n,m=map(int,input().split())
+n1=list(map(int,input().split()))
+m1=list(map(int,input().split()))
+temp=[]
+c=0
+for i in n1:
+    if (i in m1) and (i not in temp): 
+        temp.append(i)
+        c+=1
+print(c)        
