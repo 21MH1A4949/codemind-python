3af08856faf324394cbc00001fff1d5448953664
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2022-07-14 15:16:49 +0530

    n=input() r=["a","e","i","o","u"] k=["A","E","I","O","U"] temp=[] for i in n:     if (i in r or  i in k )and i not in temp:         temp.append(i)         print(i,end=" ") if not bool(len(temp)):     print(-1)

--- vowels_in__string.py
+++ vowels_in__string.py
@@ -1,11 +1,11 @@
-s = input()
-v = ['a','e','i','o','u']
-
-V = ['A','E','I','O','U']
-temp = []
-for i  in s:
-    if (i in v or i in V) and i not in temp:
+n=input()
+r=["a","e","i","o","u"]
+k=["A","E","I","O","U"]
+temp=[]
+for i in n:
+    if (i in r or  i in k )and i not in temp:
         temp.append(i)
-        print(i,end=' ')
+        print(i,end=" ")
 if not bool(len(temp)):
     print(-1)
+        
