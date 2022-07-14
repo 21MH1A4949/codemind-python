668bedb58c34d9f3373fb1a7feac406e4edfab46
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2022-07-14 19:37:39 +0530

    n=input().split() a=["a","e","i","o","u"] mc = 0 for i in n:     c=0     for j in i:         if j in a:             c+=1     if mc<c:         mc=c wc=0 for i in n:     c=0     for j in i:         if j in a:             c+=1     if c==mc:         wc+=1
    print(wc)

---
+++ count_words_with_maximum_vowels.py
@@ -0,0 +1,20 @@
+n=input().split()
+a=["a","e","i","o","u"]
+mc = 0
+for i in n:
+    c=0
+    for j in i:
+        if j in a:
+            c+=1
+    if mc<c:
+        mc=c
+wc=0
+for i in n:
+    c=0
+    for j in i:
+        if j in a:
+            c+=1
+    if c==mc:
+        wc+=1
+
+print(wc)            
