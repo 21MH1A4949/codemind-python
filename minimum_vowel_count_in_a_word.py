ddc485f93ca2f44a36264a9b99f379d3dbce9153
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2022-07-14 22:42:17 +0530

    n=input().lower().split() a="aeiou" mc=0 for i in n[0]:     if i in a:         mc+=1 for i in n:     c=0     for j in i:         if j in a:             c+=1     if mc>c:         mc=c print(mc)

--- minimum_vowel_count_in_a_word.py
+++ minimum_vowel_count_in_a_word.py
@@ -1,16 +1,14 @@
-n=input().split()
-r=["a","e","i","o","u"]
+n=input().lower().split()
+a="aeiou"
 mc=0
-
 for i in n[0]:
-    if i in r:
+    if i in a:
         mc+=1
 for i in n:
     c=0
     for j in i:
-        if j in r:
+        if j in a:
             c+=1
-    if mc>c :
+    if mc>c:
         mc=c
-print(mc)        
-    
+print(mc)
