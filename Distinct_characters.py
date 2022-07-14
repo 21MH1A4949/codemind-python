5e1d8f620118acf578a36d0385192f5a10c41a0e
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2022-07-14 14:35:51 +0530

    s = set(input().lower()) s.discard(' ') s = list(s) s.sort() for i in s:     print(i,end='')

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,9 +1,6 @@
-n=list(input().lower())
-c=0
-l =[]
-for i in n:
-    if(n.count(i)==1) and i!=' ':
-        l.append(i)
-l.sort()
-for i in l:
+s = set(input().lower())
+s.discard(' ')
+s = list(s)
+s.sort()
+for i in s:
     print(i,end='')
