8d9b0b57790221b57942c0a1076686807ec103e9
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2022-07-14 14:50:46 +0530

    s = input() v = ['a','e','i','o','u'] V = ['A','E','I','O','U']
    c = 1
    for i in v:
        if i not in s:
            c=0
            break
    if c==0:
        for i in V:
            if i not in s:
                c=0
                break
        else:
            c=1
    if c==0:
        print(False)
    else:
        print(True)

--- All_vowels_in_a_string.py
+++ All_vowels_in_a_string.py
@@ -1,22 +1,20 @@
-n=(input())
-# print(n)
-c=1
-k=["a","e","i",'o',"u"]
-f=["A","E","I","O","U"]
-for i in k:
-    if i  not in n:
+s = input()
+v = ['a','e','i','o','u']
+V = ['A','E','I','O','U']
+
+c = 1
+for i in v:
+    if i not in s:
         c=0
         break
-
-
 if c==0:
-    for i in f:
-        if i not in n:
+    for i in V:
+        if i not in s:
             c=0
             break
     else:
         c=1
-if c==1:
-    print("True")
-else :
+if c==0:
     print(False)
+else:
+    print(True)
