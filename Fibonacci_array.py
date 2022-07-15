3ac23dfff24b9d6e3bdd6142faa1dc9341260b0f
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2022-07-15 20:03:52 +0530

    n = int(input()) x = list(map(int,input().split())) f = 0 for i in range(2,n):     if x[i-2]+x[i-1]==x[i]:         f=1     else:         f=0         break if bool(f):     print('yes') else:     print('no')

--- Fibonacci_array.py
+++ Fibonacci_array.py
@@ -1,13 +1,13 @@
-n=int(input())
-x=list(map(int,input().split()))
-f=0
+n = int(input())
+x = list(map(int,input().split()))
+f = 0
 for i in range(2,n):
-    if x[i]==x[i-2]+x[i-1]:
+    if x[i-2]+x[i-1]==x[i]:
         f=1
     else:
         f=0
         break
-if f!=1:
-    print("no")
+if bool(f):
+    print('yes')
 else:
-    print("yes")
+    print('no')
