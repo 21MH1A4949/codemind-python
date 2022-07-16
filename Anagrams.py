6a190aa708a681c1c7f1a7e847dd4284065623df
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2022-07-16 18:43:32 +0530

    s1=(input()) s2=(input()) c=0 for i in s1:     if i in s2 and i in s1:         print(True)         break     else:         print(False)         break

--- Anagrams.py
+++ Anagrams.py
@@ -1,8 +1,9 @@
-s1=input()
-s2=input()
+s1=(input())
+s2=(input())
+c=0
 for i in s1:
-    if i in s2 and i  in s1:
-        print("True")
+    if i in s2 and i in s1:
+        print(True)
         break
     else:
         print(False)
