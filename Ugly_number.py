7cab09e7bba9c835fa78f74e14639ff0e3153fc4
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2022-08-23 10:10:54 +0530

    def prime(n):     if n==1:         return False     for i in range(2,int(n**.5)+1):         if n%i==0:             return False     return True n=int(input()) pr1=[] for i in range(2,n+1):     if n%i==0 and prime(i):         pr1.append(i) if (2 in pr1 or 3 in pr1 or 5 in pr1):     print("Ugly Number") elif n==1:     print("Ugly Number") else:     print("Not Ugly Number")

--- Ugly_number.py
+++ Ugly_number.py
@@ -1,21 +1,21 @@
 def prime(n):
-    factor =0
-    for i in range(1,n+1):
-        if n%i==0:
-            factor+=1
-    if (factor ==2):
-        return True
-    else:
+    if n==1:
         return False
-        
-a = int (input())
-prime1=[]
-for i in range(1,a+1):
-    if a%i==0 and prime(i):
-        prime1.append(i)
-if (2 in prime1 or 3 in prime1 or 5 in prime1 ):
+    for i in range(2,int(n**.5)+1):
+        if n%i==0:
+            return False
+    return True
+n=int(input())
+pr1=[]
+for i in range(2,n+1):
+    if n%i==0 and prime(i):
+        pr1.append(i)
+if (2 in pr1 or 3 in pr1 or 5 in pr1):
     print("Ugly Number")
-elif a==1:
+elif n==1:
     print("Ugly Number")
 else:
     print("Not Ugly Number")
+        
+        
+        
