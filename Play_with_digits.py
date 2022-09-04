02f0de8eb12c4092f43b51a847409383e3f8c621
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2022-09-04 18:34:30 +0530

    n=int(input()) x=list(map(int,input().split())) s=0 for i in x:     while (i!=0):         rem=i%10         i//=10         s+=rem print(s)

--- Play_with_digits.py
+++ Play_with_digits.py
@@ -1,10 +1,10 @@
-n = int(input())
-x = list(map(int,input().split()))
-s = 0
+n=int(input())
+x=list(map(int,input().split()))
+s=0
 for i in x:
-    temp = i 
-    while temp:
-        s+=temp%10
-        temp//=10
-
-print(s)
+    while (i!=0):
+        rem=i%10
+        i//=10
+        s+=rem
+print(s)        
+        
