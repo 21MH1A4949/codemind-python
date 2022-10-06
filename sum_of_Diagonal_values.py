59b4a254dc63c09e603266bd5c0e31574b0f0637
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2022-10-06 08:35:00 +0530

    m,n = map(int,input().split()) mat = [] for i in range(m):     t = list(map(int,input().split()))     mat.append(t) # print(mat) sum=0 temp = [] for i in range(m):     for j in range(n):         if (i==j or i+j==m-1 )and [i,j] not in temp:             temp.append([i,j])             # print(temp)             sum+= mat[i][j] print(sum)

--- sum_of_Diagonal_values.py
+++ sum_of_Diagonal_values.py
@@ -1,16 +1,15 @@
-n,m=map(int,input().split())
-mat=[]
-s=0
-for i in range(n):
-    x=list(map(int,input().split()))
-    mat.append(x)
-for i in range(n):
-    for j in range(m):
-        if i==j:
-            s+=mat[i][j]
-        elif i+j==n-1:
-            s+=mat[i][j]
-            
-            
-            
-print(s)            
+m,n = map(int,input().split())
+mat = []
+for i in range(m):
+    t = list(map(int,input().split()))
+    mat.append(t)
+# print(mat)
+sum=0
+temp = []
+for i in range(m):
+    for j in range(n):
+        if (i==j or i+j==m-1 )and [i,j] not in temp:
+            temp.append([i,j])
+            # print(temp)
+            sum+= mat[i][j]
+print(sum)
